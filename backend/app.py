43d104d96bb3c40dfd59a967dd213a07cec93110
Jay <wrbx7563@leeds.ac.uk>
2026-05-06 16:44:56 +0000

    feat: added backend CI

--- backend/app.py
+++ backend/app.py
@@ -10,13 +10,14 @@ from flask import Flask
 
 from routes.sites import sites_bp
 
-# from routes.latest import latest_bp
+from routes.latest import latest_bp
 
-# from routes.summary import summary_bp
+from routes.summary import summary_bp
 
 from routes.timeseries import timeseries_bp
 
-# from routes.alerts import alerts_bp
+from routes.alerts import alerts_bp
+
 # from routes.export import export_bp
 
 from flask_cors import CORS
@@ -24,12 +25,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(sites_bp)
-# app.register_blueprint(latest_bp)
-# app.register_blueprint(summary_bp)
+app.register_blueprint(latest_bp)
+app.register_blueprint(summary_bp)
 
 app.register_blueprint(timeseries_bp)
 
-# pp.register_blueprint(alerts_bp)
+app.register_blueprint(alerts_bp)
 # app.register_blueprint(export_bp)
 
 if __name__ == "__main__":
